721c50cb2e48d904977293156996d741903d7bab
chandkaurati <chandkaurati@outlook.com>
2024-04-24 13:11:15 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-i
+
+import conntectDB from "./db/index.js";
+import dotenv  from 'dotenv'
+
+dotenv.config({
+    path: "./env"
+})
+
+conntectDB()
+ 
+// ;(async()=>{
+//     try {
+//        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
+//     } catch (error) { 
+//       console.log(error)
+//     }
+//   })
